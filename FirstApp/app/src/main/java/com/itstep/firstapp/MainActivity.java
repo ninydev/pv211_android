69e42ecb20bb35f04f4355ace7573933b116448f
Oleksandr Nykytin <alex_n@alma-soft.com>
2025-05-17 20:19:11 +0300

    Menu

--- FirstApp/app/src/main/java/com/itstep/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/itstep/firstapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -34,7 +36,26 @@ public class MainActivity extends AppCompatActivity {
     ColorListAdapter colorListAdapter;
     ColorSpinnerAdapter colorSpinnerAdapter;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.menu_action_settings:
+//                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.menu_action_about:
+//                Toast.makeText(this, "About clicked", Toast.LENGTH_SHORT).show();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,16 +63,21 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.color_list_view);
-        Spinner spinner = findViewById(R.id.color_spinner);
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+        // getMenuInflater().inflate(R.menu.menu_main, null);
+
+
+//        ListView listView = findViewById(R.id.color_list_view);
+//        Spinner spinner = findViewById(R.id.color_spinner);
 
 
 
-        colorListAdapter = new ColorListAdapter(this, android.R.layout.simple_list_item_1);
-        listView.setAdapter(colorListAdapter);
-
-        colorSpinnerAdapter = new ColorSpinnerAdapter(this, android.R.layout.simple_spinner_item);
-        spinner.setAdapter(colorSpinnerAdapter);
+//        colorListAdapter = new ColorListAdapter(this, android.R.layout.simple_list_item_1);
+//        listView.setAdapter(colorListAdapter);
+//
+//        colorSpinnerAdapter = new ColorSpinnerAdapter(this, android.R.layout.simple_spinner_item);
+//        spinner.setAdapter(colorSpinnerAdapter);
 
 
     }
